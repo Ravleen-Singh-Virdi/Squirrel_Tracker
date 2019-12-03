58664d58a3e8f389e84e6d4d509a0148e0ee94ef
Ravleen Virdi <rv2428@columbia.edu>
2019-12-03 21:27:07 +0000

    creating views file

--- Squirrel_Sightings/views.py
+++ Squirrel_Sightings/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 import random
+from django.shortcuts import render, get_object_or_404, redirect
+from . models import Squirrels
 
 def index(request):
-    i = random.random()
-    return HttpResponse(f"Welcome!{i}")
+    context = {"Squirrel Sightings": Squirrels.objects.all(), "field_names": Squirrels._meta.get_fields()}
+    return render(request, 'Squirrel_Sightings/index.html',context)
 
 # Create your views here.
