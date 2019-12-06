ebf90c4e6e7dd6b9cbb48f6173a9e11ebf421578
Ravleen Virdi <rv2428@columbia.edu>
2019-12-06 05:14:07 +0000

    doing some changes

--- Squirrel_Sightings/views.py
+++ Squirrel_Sightings/views.py
@@ -7,13 +7,6 @@ def index(request):
     context = {"sightings": Squirrels.objects.all(), "field_names": Squirrels._meta.get_fields()}
     return render(request, 'Squirrel_Sightings/index.html',context)
 
-def sightings(request):
-    squirrels = Squirrels.objects.all()
-    context = {
-            "squirrels": squirrels,
-        }
-    return render(request, 'Squirrel_Sightings/sightings.html', context)
-
 def coordinates(request):
     squirrels  = Squirrels.objects.all()[:100]
     context = {
