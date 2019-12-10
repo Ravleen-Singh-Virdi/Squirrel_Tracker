d866b42e40a7653482968c75c86c6831d370adc8
Ravleen Virdi <rv2428@columbia.edu>
2019-12-10 04:40:09 +0000

    when will the final deploy come?

--- Squirrel_Sightings/views.py
+++ Squirrel_Sightings/views.py
@@ -4,7 +4,7 @@ from . models import Squirrels
 from .forms import SquirrelForm
 
 def landing(request):
-    return render(request, 'landing.html')
+    return render(request, 'Squirrel_Sightings/landing.html')
 
 def view_all(request):
     context = {
--- Squirrel_Tracker/urls.py
+++ Squirrel_Tracker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('squirrel_sightings/', include('Squirrel_Sightings.urls')),   
+    path('', include('Squirrel_Sightings.urls')),   
     path('admin/', admin.site.urls),
 ]
